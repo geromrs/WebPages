a7039bb6e87c0a84028c12cc1d45460bd27d7c13
Gerardo Muriel <geromrs20@gmail.com>
2021-12-09 09:32:21 -0800

    Paginas Web

---
+++ WF/js/configuracion-typed.js
@@ -0,0 +1,21 @@
+var typed = new Typed('.typed', {
+    strings: [
+    '<i class="wf">WhiteFrog</i>',
+    '<i class="wf">Creamos</i>',
+    '<i class="wf">Revisamos</i>',
+    '<i class="wf">Respaldamos</i>',
+    '<i class="wf">Mejoramos</i>'
+    ],
+    stringsElement: '#cadenas-texto',
+    typeSpeed: 75,
+    startDelay: 300,
+    backSpeed: 75,
+    smartBackspace: true,
+    shuffle: false,
+    backDelay: 1500,
+    loop: true,
+    loopCount: false,
+    showCursor: true,
+    cursorChar: '|',
+    contentType: 'html',
+})
